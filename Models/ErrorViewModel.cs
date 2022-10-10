e90a34fcf005d2c58ea23d4659466bd7918cbc6e
Janek <Janek@DESKTOP-JST2FPF>
2022-10-10 17:55:02 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlgorytmGenetyczny.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
