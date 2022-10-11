11a022e27583e60cac6edfb029e611a259cc0c9c
Jan <janjrybicki@gmail.com>
2022-10-11 16:30:40 +0200

    Added XModel

---
+++ Models/XModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgorytmGenetyczny.Models
+{
+    public class XModel
+    {
+        public double RangeBegining { get; set; }
+        public double RangeEnd { get; set; }
+        public double Accuracy { get; set; }
+        public int Number { get; set; }
+        public double XReal1 { get; set; }
+        public double XInt1 { get; set; }
+        public string XBin { get; set; }
+        public double XInt2 { get; set; }
+        public double XReal2 { get; set; }
+        public double FunctionValue { get; set; }
+    }
+}
