a050acb2106aac374b0e00bddf3ed55b394ee169
Jan <janjrybicki@gmail.com>
2022-10-16 18:57:51 +0200

    Changed the default controller

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=X}/{action=Index}/{id?}");
 
 app.Run();
