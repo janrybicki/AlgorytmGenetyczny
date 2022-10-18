8481aae069a5c762e4cd7631420620ddb44e28f3
Jan <janjrybicki@gmail.com>
2022-10-18 23:56:56 +0200

    Fixed mistakes in model

--- Models/XModel.cs
+++ Models/XModel.cs
@@ -2,15 +2,16 @@
 {
     public class XModel
     {
-        public double RangeBegining { get; set; }
+        public double RangeBeginning { get; set; }
         public double RangeEnd { get; set; }
         public double Accuracy { get; set; }
         public int Number { get; set; }
         public double XReal1 { get; set; }
-        public double XInt1 { get; set; }
+        public int XInt1 { get; set; }
         public string XBin { get; set; }
-        public double XInt2 { get; set; }
+        public int XInt2 { get; set; }
         public double XReal2 { get; set; }
         public double FunctionValue { get; set; }
+
     }
 }
