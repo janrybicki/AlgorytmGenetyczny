cec4b4e649af215c1a498e345c1179a23fcafeb6
Jan <janjrybicki@gmail.com>
2022-10-18 23:57:46 +0200

    Added a controller and views

---
+++ Controllers/XController.cs
@@ -0,0 +1,59 @@
+﻿using AlgorytmGenetyczny.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlgorytmGenetyczny.Controllers
+{
+    public class XController : Controller
+    {
+        private static IList<XModel> x = new List<XModel>();
+        // GET: XController
+        public ActionResult Index()
+        {
+            return View(x);
+        }
+
+        // GET: XController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: XController/Create 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: XController/Create
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(XModel xModel)//5.09801
+        {
+            var random = new Random();
+            var l = Math.Ceiling(Math.Log2((xModel.RangeEnd - xModel.RangeBeginning) / xModel.Accuracy + 1));
+            for (int i = 0; i < xModel.Number; i++)
+            {
+                var randomNumber = random.NextDouble() * (xModel.RangeEnd - xModel.RangeBeginning) + xModel.RangeBeginning;
+                var xReal1 = randomNumber - randomNumber % xModel.Accuracy;
+                var xInt1 = (int)Math.Round(1 / (xModel.RangeEnd - xModel.RangeBeginning) * (xReal1 - xModel.RangeBeginning) * (Math.Pow(2, l) - 1));
+                var xBin = Convert.ToString(xInt1, 2);
+                var xInt2 = Convert.ToInt32(xBin, 2);
+                var xReal2 = xInt2 * (xModel.RangeEnd - xModel.RangeBeginning) / (Math.Pow(2, l) - 1) + xModel.RangeBeginning;
+                var xReal2Roundend = Math.Abs(xReal2 % xModel.Accuracy) < xModel.Accuracy / 2 ? xReal2 - xReal2 % xModel.Accuracy : xReal2 + Math.Sign(xReal2) * xModel.Accuracy - xReal2 % xModel.Accuracy;
+                var functionValue = (xReal2Roundend % 1) * (Math.Cos(xReal2Roundend * 20 * Math.PI) - Math.Sin(xReal2Roundend));
+                var functionValueRounded = Math.Abs(functionValue % xModel.Accuracy) < xModel.Accuracy / 2 ? functionValue - functionValue % xModel.Accuracy : functionValue + Math.Sign(functionValue) * xModel.Accuracy - functionValue % xModel.Accuracy;
+                x.Add(new XModel()
+                {
+                    XReal1 = xReal1,
+                    XInt1 = xInt1,
+                    XBin = xBin,
+                    XInt2 = xInt2,
+                    XReal2 = xReal2Roundend,
+                    FunctionValue = functionValueRounded
+                });
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
