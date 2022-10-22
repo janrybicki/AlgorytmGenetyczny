9b2d7725ca6de0f8bcd5f87bd28a2db02d11e392
Jan <janjrybicki@gmail.com>
2022-10-22 21:48:46 +0200

    Fixed rounding

--- Controllers/XController.cs
+++ Controllers/XController.cs
@@ -30,24 +30,28 @@ namespace AlgorytmGenetyczny.Controllers
             for (int i = 0; i < xModel.Number; i++)
             {
                 var randomNumber = random.NextDouble() * (xModel.RangeEnd - xModel.RangeBeginning) + xModel.RangeBeginning;
-                var xReal1 = randomNumber - randomNumber % xModel.Precision;
+                var xReal1 = Math.Abs(randomNumber % xModel.Precision) < xModel.Precision / 2
+                    ? randomNumber - randomNumber % xModel.Precision
+                    : randomNumber + Math.Sign(randomNumber) * xModel.Precision - randomNumber % xModel.Precision;
                 var xInt1 = (int)Math.Round(1 / (xModel.RangeEnd - xModel.RangeBeginning) * (xReal1 - xModel.RangeBeginning) * (Math.Pow(2, l) - 1));
                 var sb = new StringBuilder(Convert.ToString(xInt1, 2));
                 sb.Insert(0, new String('0', l - sb.Length));
                 var xBin = sb.ToString();
                 var xInt2 = Convert.ToInt32(xBin, 2);
                 var xReal2 = xInt2 * (xModel.RangeEnd - xModel.RangeBeginning) / (Math.Pow(2, l) - 1) + xModel.RangeBeginning;
-                var xReal2Roundend = Math.Abs(xReal2 % xModel.Precision) < xModel.Precision / 2 ? xReal2 - xReal2 % xModel.Precision : xReal2 + Math.Sign(xReal2) * xModel.Precision - xReal2 % xModel.Precision;
+                var xReal2Roundend = Math.Abs(xReal2 % xModel.Precision) < xModel.Precision / 2
+                    ? xReal2 - xReal2 % xModel.Precision
+                    : xReal2 + Math.Sign(xReal2) * xModel.Precision - xReal2 % xModel.Precision;
                 var functionValue = (xReal2Roundend % 1) * (Math.Cos(xReal2Roundend * 20 * Math.PI) - Math.Sin(xReal2Roundend));
-                //var functionValueRounded = Math.Abs(functionValue % xModel.Precision) < xModel.Precision / 2 ? functionValue - functionValue % xModel.Precision : functionValue + Math.Sign(functionValue) * xModel.Precision - functionValue % xModel.Precision;
+
                 x.Add(new XModel(xModel)
                 {
-                    XReal1 = xReal1,
+                    XReal1 = (float)xReal1,
                     XInt1 = xInt1,
                     XBin = xBin,
                     XInt2 = xInt2,
-                    XReal2 = xReal2Roundend,
-                    FunctionValue = functionValue
+                    XReal2 = (float)xReal2Roundend,
+                    FunctionValue = (float)functionValue
                 });
             }
             return RedirectToAction(nameof(Index));
--- Models/XModel.cs
+++ Models/XModel.cs
@@ -5,15 +5,15 @@ namespace AlgorytmGenetyczny.Models
     public class XModel
     {
         [DisplayName("a")]
-        public double RangeBeginning { get; set; }
+        public float RangeBeginning { get; set; }
         [DisplayName("b")]
-        public double RangeEnd { get; set; }
+        public float RangeEnd { get; set; }
         [DisplayName("d")]
         public double Precision { get; set; }
         [DisplayName("N")]
         public int Number { get; set; }
         [DisplayName("xReal")]
-        public double XReal1 { get; set; }
+        public float XReal1 { get; set; }
         [DisplayName("xInt")]
         public int XInt1 { get; set; }
         [DisplayName("xBin")]
@@ -21,9 +21,9 @@ namespace AlgorytmGenetyczny.Models
         [DisplayName("xInt")]
         public int XInt2 { get; set; }
         [DisplayName("xReal")]
-        public double XReal2 { get; set; }
+        public float XReal2 { get; set; }
         [DisplayName("f(x)")]
-        public double FunctionValue { get; set; }
+        public float FunctionValue { get; set; }
 
         public XModel(XModel xModel)
         {
@@ -31,6 +31,6 @@ namespace AlgorytmGenetyczny.Models
             this.RangeEnd = xModel.RangeEnd;
             this.Precision = xModel.Precision;
         }
-        public XModel() {}
+        public XModel() { }
     }
 }
