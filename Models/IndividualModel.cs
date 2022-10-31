a994427cab8d9143c818c108b46424b8ad22c2ec
Jan <janjrybicki@gmail.com>
2022-10-31 12:46:14 +0100

    Added new methods to IndividualModel

--- Models/IndividualModel.cs
+++ Models/IndividualModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Text;
 
 namespace AlgorytmGenetyczny.Models
 {
@@ -22,7 +24,7 @@ namespace AlgorytmGenetyczny.Models
         public int XInt2 { get; set; }
         [DisplayName("xReal")]
         public float XReal2 { get; set; }
-        [DisplayName("f(x)")]
+        [DisplayName("f(x)")]  
         public float FunctionValue { get; set; }
 
         public IndividualModel(IndividualModel xModel)
@@ -32,5 +34,41 @@ namespace AlgorytmGenetyczny.Models
             this.Precision = xModel.Precision;
         }
         public IndividualModel() { }
+
+        public static double GenerateRandomXReal(double rangeBeginning, double rangeEnd)
+        {
+            var random = new Random();
+            return random.NextDouble() * (rangeEnd - rangeBeginning) + rangeBeginning;
+        }
+        public static double Function(double xReal)
+        {
+            return (xReal % 1) * (Math.Cos(xReal * 20 * Math.PI) - Math.Sin(xReal));
+        }
+        public static double Round(double xReal, double precision)
+        {
+            return Math.Abs(xReal % precision) < precision / 2
+                ? xReal - xReal % precision
+                    : xReal + Math.Sign(xReal) * precision - xReal % precision;
+        }
+
+        public static int XRealToXInt(double xReal, int binaryLength, double rangeBeginning, double rangeEnd)
+        {
+            return (int)Math.Round(1 / (rangeEnd - rangeBeginning) * (xReal - rangeBeginning) * (Math.Pow(2, binaryLength) - 1));
+        }
+
+        public static string XIntToXBin(int xInt, int binaryLength)
+        {
+            var sb = new StringBuilder(Convert.ToString(xInt, 2));
+            sb.Insert(0, new String('0', binaryLength - sb.Length));
+            return sb.ToString();
+        }
+        public static int XBinToXInt(string xBin)
+        {
+            return Convert.ToInt32(xBin, 2);
+        }
+        public static double XIntToXReal(int xInt, double rangeBeginning, double rangeEnd, int binaryLength)
+        {
+            return xInt * (rangeEnd - rangeBeginning) / (Math.Pow(2, binaryLength) - 1) + rangeBeginning;
+        }
     }
 }
--- Services/PopulationService.cs
+++ Services/PopulationService.cs
@@ -13,29 +13,20 @@ namespace AlgorytmGenetyczny.Services
         public void CreatePopulation(PopulationModel populationModel)
         {
             Population = populationModel;
-            var random = new Random();
             var l = (int)Math.Ceiling(Math.Log2((populationModel.RangeEnd - populationModel.RangeBeginning) / populationModel.Precision + 1));
             for (int i = 0; i < populationModel.Number; i++)
             {
-                var randomNumber = random.NextDouble() * (populationModel.RangeEnd - populationModel.RangeBeginning) + populationModel.RangeBeginning;
-                var xReal1 = Math.Abs(randomNumber % populationModel.Precision) < populationModel.Precision / 2
-                ? randomNumber - randomNumber % populationModel.Precision
-                    : randomNumber + Math.Sign(randomNumber) * populationModel.Precision - randomNumber % populationModel.Precision;
-                var xInt1 = (int)Math.Round(1 / (populationModel.RangeEnd - populationModel.RangeBeginning) * (xReal1 - populationModel.RangeBeginning) * (Math.Pow(2, l) - 1));
-                var sb = new StringBuilder(Convert.ToString(xInt1, 2));
-                sb.Insert(0, new String('0', l - sb.Length));
-                var xBin = sb.ToString();
-                var xInt2 = Convert.ToInt32(xBin, 2);
-                var xReal2 = xInt2 * (populationModel.RangeEnd - populationModel.RangeBeginning) / (Math.Pow(2, l) - 1) + populationModel.RangeBeginning;
-                var xReal2Roundend = Math.Abs(xReal2 % populationModel.Precision) < populationModel.Precision / 2
-                ? xReal2 - xReal2 % populationModel.Precision
-                    : xReal2 + Math.Sign(xReal2) * populationModel.Precision - xReal2 % populationModel.Precision;
-                var functionValue = (Math.IEEERemainder(xReal2Roundend, 1)) * (Math.Cos(xReal2Roundend * 20 * Math.PI) - Math.Sin(xReal2Roundend));
-                //rozwazyc powrot do %
-                Population = populationModel;
+                var xReal1 = IndividualModel.GenerateRandomXReal(populationModel.RangeBeginning, populationModel.RangeEnd);
+                var xReal1Rounded = IndividualModel.Round(xReal1, populationModel.Precision);
+                var xInt1 = IndividualModel.XRealToXInt(xReal1Rounded, l, populationModel.RangeBeginning, populationModel.RangeEnd);
+                var xBin = IndividualModel.XIntToXBin(xInt1, l);
+                var xInt2 = IndividualModel.XBinToXInt(xBin);
+                var xReal2 = IndividualModel.XIntToXReal(xInt2, populationModel.RangeBeginning, populationModel.RangeEnd, l);
+                var xReal2Roundend = IndividualModel.Round(xReal2, populationModel.Precision);
+                var functionValue = IndividualModel.Function(xReal2Roundend);
                 Population.Individuals.Add(new IndividualModel()
                 {
-                    XReal1 = (float)xReal1,
+                    XReal1 = (float)xReal1Rounded,
                     XInt1 = xInt1,
                     XBin = xBin,
                     XInt2 = xInt2,
